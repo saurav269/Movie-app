06fe266b8f707912934fa99f01bb160e561f558b
Saurav Mallik <sauravmallik786@gmail.com>
2023-12-05 17:37:32 +0530

    error handle

--- vite-project/src/pages/404/PagenotFound.jsx
+++ vite-project/src/pages/404/PagenotFound.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import './style.scss';
+import ContentWrapper from '../../components/contentWrapper/ContentWrapper';
 const PagenotFound = () => {
   return (
-    <div>
-      
+    <div className="pageNotFound">
+      <ContentWrapper>
+        <span className="bigText">404</span>
+        <span className="smallText">Page not found!</span>
+      </ContentWrapper>
     </div>
-  )
+  );
 }
 
 export default PagenotFound
